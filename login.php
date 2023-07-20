2ae63353bfcf87cf58411a0f29aba16ddc19f72a
bunlong22 <suonbunlong93@gmail.com>
2023-07-20 23:15:17 +0700

    create input for username, pw to login

--- login.php
+++ login.php
@@ -1,13 +1,18 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Login</title>
 </head>
-
 <body>
     <h2>Login</h2>
-
+    <form action="login.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username">
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password">
+        <br>
+        <input type="submit" value="Login">
+    </form>
 </body>
-
 </html>
