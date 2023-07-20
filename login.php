4a28e082a57b64d09e5e3b31daca234c9f50f038
bunlong22 <suonbunlong93@gmail.com>
2023-07-20 23:23:46 +0700

    add the link for bootstrap

--- login.php
+++ login.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html>
+
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>Login</title>
 </head>
+
 <body>
-    <h2>Login</h2>
+<h2>Login</h2>
     <form action="login.php" method="post">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username">
