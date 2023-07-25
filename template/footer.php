7583b5d28339a0bb6ad24bf79bd7f8a814a698e5
bunlong22 <suonbunlong93@gmail.com>
2023-07-25 23:31:31 +0700

    finalize the footer

---
+++ template/footer.php
@@ -0,0 +1,20 @@
+<hr>
+
+<footer class="fixed-bottom bg-light bg-gradient border py-3 px-2">
+	<div class="container">
+		<div class="d-flex justify-content-center">
+			<div class="">
+				<a class="text-decoration-none text-muted fw-bold">
+					E-Book Shop &copy;
+					<?= date('Y') ?>
+				</a>
+			</div>
+		</div>
+	</div>
+</footer>
+<div class="clear-fix py-4"></div>
+</div>
+
+</body>
+
+</html>
