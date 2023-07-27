4d5b75a32a63f6eba1e15a60e06ea057ed666310
bunlong22 <suonbunlong93@gmail.com>
2023-07-27 12:43:11 +0700

    finalize the index page

--- index.php
+++ index.php
@@ -1,34 +1,42 @@
 <?php
 session_start();
 $count = 0;
-// connecto database
-
 $title = "Home";
 require_once "./template/header.php";
 require_once "./functions/database_functions.php";
 $conn = db_connect();
 $row = select4LatestBook($conn);
 ?>
-<!-- Example row of columns -->
 <div class="lead text-center text-dark fw-bolder h4">Latest books</div>
 <center>
   <hr class="bg-warning" style="width:5em;height:3px;opacity:1">
 </center>
 <div class="row">
   <?php foreach ($row as $book) { ?>
-    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 py-2 mb-2">
+    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 py-2 mb-2 ">
       <a href="book-details.php?bookisbn=<?php echo $book['book_isbn']; ?>"
         class="card rounded-0 shadow book-item text-reset text-decoration-none">
-        <div class="img-holder overflow-hidden">
-          <img class="img-top" src="./public/img/<?php echo $book['book_image']; ?>">
-        </div>
+        <img class="img-top" style="height: 250px;" src="./public/img/<?php echo $book['book_image']; ?>">
         <div class="card-body">
-          <div class="card-title fw-bolder h5 text-center">
-            <?= $book['book_title'] ?>
+          <div class="card-title fw-bolder h6 text-center">
+            <?php
+            $maxTitleLength = 40;
+            $title = $book['book_title'];
+            if (strlen($title) > $maxTitleLength) {
+              $title = substr($title, 0, $maxTitleLength) . '...';
+            }
+            echo $title;
+            ?>
+            <div class="text-center">
+              <span class="h6 text-danger">$
+                <?= $book['book_price'] ?>
+              </span>
+            </div>
           </div>
         </div>
       </a>
     </div>
+
   <?php } ?>
 </div>
 <?php
@@ -37,3 +45,13 @@ if (isset($conn)) {
 }
 require_once "./template/footer.php";
 ?>
+<style>
+  .card-body {
+    height: 100px;
+    overflow: hidden;
+  }
+
+  .card:hover {
+    border: 2px solid blue;
+  }
+</style>
